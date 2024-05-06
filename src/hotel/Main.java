aad5f3384d59e101af9731071e57bab9cac3b96a
phxdablio <pedro.11gustavo.7@gmail.com>
2024-05-06 12:19:54 -0300

    puxando cod

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -1,16 +1,11 @@
 package hotel;
 
-<<<<<<< HEAD
+
 
 public class Main {
     public static void main(String[] args) {
     	System.out.println("OI");
 
     }
-=======
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("OI");
-	}
->>>>>>> 43647aa9430a8e5e7e45de1bce08ff0695363530
+
 }
