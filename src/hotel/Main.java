1345d8fe001b83127fbc9fa4e23a2bb469a5a5d9
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-05-06 12:08:24 -0300

    classe base de hospede adicionada

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -1,7 +1,14 @@
 package hotel;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("OI");
+	
+	private static int NUMERO_QUARTOS = 10;
+	private static int NUMERO_HOSPEDES = 50;
+	
+	public static void main(String[] args) {	
+		// Crie e inicie as threads para os Hóspedes
+        for (int i = 0; i < 50; i++) {
+            new Hospedes("Hóspede " + (i + 1), 1, null).start(); // Crie e inicie 50 Hóspedes
+        }
 	}
 }
